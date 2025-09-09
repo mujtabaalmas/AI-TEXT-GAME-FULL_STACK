55375d0a05ffb66670553c39065835941c8f1e75
mujtabaalmas <kingmujtaba30@gmail.com>
2025-09-09 19:55:15 +0500

    Completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,15 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig( (command, mode) => {
-  const env = loadEnv(mode, process.cwd(),"")
+export default defineConfig((command, mode) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
   console.log(env.VITE_DEBUG)
 
   return {
-  plugins: [react()],
+      plugins: [react()],
   server: {
-    ...command(env.VITE_DEBUG === "true" && {
+    ...(env.VITE_DEBUG === "true" && {
        proxy: {
       "/api": {
         target: "http://localhost:8000",
